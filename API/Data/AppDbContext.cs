72eaa35becbf23d0444645762cf90da8a572f451
jitjose_ghouse <jitjose@guidehouse.com>
2025-10-01 19:39:24 +0530

    Main : Setting up skeleton for datingApp

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace API.Data;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    // public AppDbContext(DbContextOptions options) : base(options)
+    public DbSet<AppUser> users { get; set; }
+
+}
---
+++ API/Data/SQLQuery/INSERT INTO users (Id, DisplayName, Emai.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (Id, DisplayName, Email)
+VALUES ('bob-id', 'Bob', 'bob@test.com');
+
+INSERT INTO users (Id, DisplayName, Email)
+VALUES ('tom-id', 'Tom', 'tom@test.com');
+
+INSERT INTO users (Id, DisplayName, Email)
+VALUES ('margert-id', 'Margert', 'margert@test.com');
+
+INSERT INTO users (Id, DisplayName, Email)
+VALUES ('sona-id', 'Sona', 'sona@test.com');
+
+
+SELECT * FROM users;
